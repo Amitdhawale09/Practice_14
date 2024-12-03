1171783964d5682e879948997e412187290e3a8d
Amitdhawale09 <104753921+Amitdhawale09@users.noreply.github.com>
2024-12-03 11:20:43 +0530

    Add files via upload

---
+++ Assign.py
@@ -0,0 +1,16 @@
+def assign():
+    print("helloo...")
+    st=input("Enter the file name..")
+    print(st)
+    
+       # fp=open('r','r')
+       # data=fp.read()
+       # print(data)
+    
+
+def main():
+    print("Inside main function..")
+    assign()
+
+if __name__ =="__main__":
+    main()
